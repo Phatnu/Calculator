643fdb24e0e815b0b73cde06ecce0383a76b4f86
Phatnu <93328910+Phatnu@users.noreply.github.com>
2024-03-31 05:55:54 +0800

    Add files via upload

---
+++ Calculatorjs/js.js
@@ -0,0 +1,48 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+//CALCULATOR FUNCTION
+// function calculate(){
+//     try{
+//         display.value = eval(display.value);
+//     }
+//     catch(error){
+//         display.value = "Error";
+//     }
+// }
+
+//PRANK FUNCTION
+function calculate(){
+    try{
+        display.value = "I Love you!";
+        display.style.transition = "2s ease";
+        display.style.color = "black";
+        display.style.border = "2px solid black";
+        display.style.textAlign = "center";
+        display.style.fontSize = "2.5rem";
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+//ADD SPEECH FOR CALCULATOR
+let btn = document.getElementById("btnsounds");
+let text = document.getElementById("display");
+btn.addEventListener("click", () =>{
+let utterance = new
+SpeechSynthesisUtterance(text.value);
+speechSynthesis.speak(utterance);
+});
+//MOVE BUTTON
+let btn2 = document.getElementById("clr");
+let position = 0;
+btn2.addEventListener("mouseover", () => {
+    position = position === 0 ? 200 : 0;
+    btn2.style.transform = `translate(${position}px)`; // Change btn to btn2
+});
