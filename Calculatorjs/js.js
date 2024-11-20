54b827720c1099e6a8d456fccd15391d7adfaa0e
Phatnu <cuetop175@gmail.com>
2024-11-20 09:30:58 +0800

    Update Js
    Update Js

--- Calculatorjs/js.js
+++ Calculatorjs/js.js
@@ -8,29 +8,29 @@ function clearDisplay(){
     display.value = "";
 }
 //CALCULATOR FUNCTION
-// function calculate(){
-//     try{
-//         display.value = eval(display.value);
-//     }
-//     catch(error){
-//         display.value = "Error";
-//     }
-// }
-
-//PRANK FUNCTION
 function calculate(){
     try{
-        display.value = "I Love you gar!";
-        display.style.transition = "2s ease";
-        display.style.color = "black";
-        display.style.border = "2px solid black";
-        display.style.textAlign = "center";
-        display.style.fontSize = "2.5rem";
+        display.value = eval(display.value);
     }
     catch(error){
         display.value = "Error";
     }
 }
+
+//PRANK FUNCTION
+// function calculate(){
+//     try{
+//         display.value = "I Love you gar!";
+//         display.style.transition = "2s ease";
+//         display.style.color = "black";
+//         display.style.border = "2px solid black";
+//         display.style.textAlign = "center";
+//         display.style.fontSize = "2.5rem";
+//     }
+//     catch(error){
+//         display.value = "Error";
+//     }
+// }
 //ADD SPEECH FOR CALCULATOR
 let btn = document.getElementById("btnsounds");
 let text = document.getElementById("display");
@@ -39,10 +39,10 @@ let utterance = new
 SpeechSynthesisUtterance(text.value);
 speechSynthesis.speak(utterance);
 });
-//MOVE BUTTON
-let btn2 = document.getElementById("clr");
-let position = 0;
-btn2.addEventListener("mouseover", () => {
-    position = position === 0 ? 200 : 0;
-    btn2.style.transform = `translate(${position}px)`; // Change btn to btn2
-});
+//IF GUSTO MO PAG NAGHOVER SA C MAGMOMOVE
+// let btn2 = document.getElementById("clr");
+// let position = 0;
+// btn2.addEventListener("mouseover", () => {
+//     position = position === 0 ? 200 : 0;
+//     btn2.style.transform = `translate(${position}px)`; // Change btn to btn2
+// });
